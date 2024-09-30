bfc6c177ebc874fb6c87a1145c92b1a9875fb94a
Amoghavarsha-Rao <amoghavarsharao99@gmail.com>
2024-09-29 21:32:32 -0500

    updated import statement

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./components/navbar";
+import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Frameworks from "./components/Frameworks";
